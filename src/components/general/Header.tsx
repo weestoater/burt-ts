2daa0153faaf9432124095c65ccf1fff48be695f
Ian Burrett <ian@weestoater.com>
2023-05-20 15:18:34 +0100

    header tweaks

--- src/components/general/Header.tsx
+++ src/components/general/Header.tsx
@@ -4,12 +4,15 @@ interface HeaderProps {
 }
 
 export default function Header(props: HeaderProps) {
-  
   return (
-    <nav className="navbar navbar-expand-lg bg-light navbar-light" data-bs-theme="light" id="top">
+    <nav
+      className="navbar navbar-expand-lg bg-light navbar-light"
+      data-bs-theme="light"
+      id="top"
+    >
       <div className="navbar-brand">
-      <i className="bi bi-journal-code mx-2" />
-        {props.site}
+        <img src="assets/burt.png" className="logo" alt="burt logo" />
+        <a href="/">{props.site}</a>
       </div>
       <button
         className="navbar-toggler"
@@ -27,12 +30,31 @@ export default function Header(props: HeaderProps) {
 
       <div className="collapse navbar-collapse" id="topNavBar">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
-          <li className="nav-item"><a className="nav-link" href="/about">About</a></li>
-          <li className="nav-item"><a className="nav-link" href="/a11y">A11y</a></li>
-          <li className="nav-item"><a className="nav-link" href="/agile">Agile</a></li>
-          <li className="nav-item"><a className="nav-link" href="/football">Football</a></li>
-          <li className="nav-item"><a className="nav-link" href="/react">React</a></li>
+          <li className="nav-item">
+            <a className="nav-link" href="/about">
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/a11y">
+              A11y
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/agile">
+              Agile
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/football">
+              Football
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/react">
+              React
+            </a>
+          </li>
         </ul>
       </div>
     </nav>
