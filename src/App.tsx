0149e9f0716b3269fc53cf8880d13c4a6a53a4ff
Ian Burrett <ian@weestoater.com>
2023-05-20 13:51:24 +0100

    css changes for main width

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
 import { Header } from './components/Header';
 import Home from "./pages/Home";
 import A11y from './pages/A11y';
