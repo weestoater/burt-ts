2767cfd51e15f233fb488f9c62f59989dee208bb
Ian Burrett <ian@weestoater.com>
2023-06-08 14:51:13 +0100

    Tidy up

--- src/components/football/ShowSeasonGoalScorers.tsx
+++ src/components/football/ShowSeasonGoalScorers.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { AgChartsReact } from "ag-charts-react";
 
 export default function ShowSeasonGoalScorers(props: any) {
+  const [goals, setGoals] = useState(0);
+  const [player, setPlayer] = useState("");
+
+  // function filterItems(_goals: number, _player: string) {
+  //   setGoals(_goals);
+  //   setPlayer(_player);
+
+  //   if (goals !== 0 && player !== "") {
+  //     //  console.log("Goals -> " + _goals + " | Player -> " + _player);
+  //     document.getElementById("modalBtn")?.click();
+  //   }
+  // }
+
   const _goalscorers: any = props.goalscorers;
 
   let _options: any = {
@@ -29,17 +42,13 @@ export default function ShowSeasonGoalScorers(props: any) {
         innerCircle: {
           fill: "#ececec",
         },
-        listeners: {
-          nodeClick: (event: any) => {
-            var datum = event.datum;
-            window.alert(
-              datum[event.calloutLabelKey] +
-                " has scored " +
-                datum[event.angleKey] +
-                " goals"
-            );
-          },
-        },
+        // listeners: {
+        //   nodeClick: (event: any) => {
+        //     var datum = event.datum;
+
+        //     filterItems(datum[event.angleKey], datum[event.calloutLabelKey]);
+        //   },
+        // },
       },
     ],
     padding: {
@@ -50,13 +59,59 @@ export default function ShowSeasonGoalScorers(props: any) {
     },
     legend: {
       position: "bottom",
-      spacing: 25,
+      spacing: 20,
     },
     theme: {},
   };
+
   return (
-    <div className="goal-scorers">
-      {_goalscorers !== null && <AgChartsReact options={_options} />}
-    </div>
+    <>
+      <div className="goal-scorers">
+        {_goalscorers !== null && <AgChartsReact options={_options} />}
+      </div>
+
+      <button
+        id="modalBtn"
+        type="button"
+        className="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#playerModal"
+      >
+        .
+      </button>
+
+      <div
+        className="modal fade"
+        id="playerModal"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="staticBackdropLabel">
+                Player details
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <p>
+                Player chosen is <b>{player}</b>
+              </p>
+              <p>
+                Number of goals they scored is <b>{goals}</b>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
