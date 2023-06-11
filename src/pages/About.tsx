efed24b5c63fca842848231d6ac8d3378252b975
Ian Burrett <ian@weestoater.com>
2023-06-11 17:54:29 +0100

    adding bio section to about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import AboutContent from "../components/about/AboutContent";
+import BioContent from "../components/about/BioContent";
 
 export default function About() {
   return (
     <>
       <div className="row">
-        <div className="col-sm-6">
+        <div className="col-sm-12">
           <h1>About</h1>
+        </div>
+        <div className="col-sm-6">
           <AboutContent />
         </div>
-        <div className="col-sm-6"></div>
+        <div className="col-sm-6">
+          <BioContent />
+        </div>
       </div>
     </>
   );
