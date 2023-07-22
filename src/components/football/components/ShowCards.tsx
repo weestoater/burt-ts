091b1a88b7886d4e2dbac61a529bef09d4fe45ef
Ian Burrett <ian@weestoater.com>
2023-07-22 19:10:22 +0100

    queens park result

--- src/components/football/components/ShowCards.tsx
+++ src/components/football/components/ShowCards.tsx
@@ -9,8 +9,8 @@ export default function ShowCards(props: any) {
         {_cards.map((card: any, index: number) => {
           return (
             <li key={index}>
-              <div className={`cards ${card.card}`}></div> {card.mins} mins
-              &middot; {card.player}
+              <div className={`cards ${card.card}`}></div>
+              <b>{card.player}</b>: {card.mins} mins
             </li>
           );
         })}
