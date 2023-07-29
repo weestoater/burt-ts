1990801b0d7fc447c0c93e40a7baf22ddd6525df
Ian Burrett <ian@weestoater.com>
2023-07-29 16:09:13 +0100

    refactored dynamic tabs

--- src/components/football/ShowTabs.tsx
+++ src/components/football/ShowTabs.tsx
@@ -10,221 +10,36 @@ export default function ShowTabs(props: any) {
 
   return (
     <>
-      <ul className="nav nav-pills" id="myTab" role="tablist">
-        <li className="nav-item" role="presentation">
-          <button
-            className="nav-link active"
-            id="twentyfour-tab"
-            data-bs-toggle="tab"
-            data-bs-target="#twentyfour"
-            type="button"
-            role="tab"
-            aria-controls="2023-24"
-            aria-selected="true"
-          >
-            2023-24
-          </button>
-        </li>
-        <li className="nav-item" role="presentation">
-          <button
-            className="nav-link"
-            id="twentythree-tab"
-            data-bs-toggle="tab"
-            data-bs-target="#twentythree"
-            type="button"
-            role="tab"
-            aria-controls="2022-23"
-            aria-selected="false"
-          >
-            2022-23
-          </button>
-        </li>
-        <li className="nav-item" role="presentation">
-          <button
-            className="nav-link"
-            id="twentytwo-tab"
-            data-bs-toggle="tab"
-            data-bs-target="#twentytwo"
-            type="button"
-            role="tab"
-            aria-controls="twentytwo"
-            aria-selected="false"
-          >
-            2021-22
-          </button>
-        </li>
-        <li className="nav-item" role="presentation">
-          <button
-            className="nav-link"
-            id="twentyone-tab"
-            data-bs-toggle="tab"
-            data-bs-target="#twentyone"
-            type="button"
-            role="tab"
-            aria-controls="twentyone"
-            aria-selected="false"
-          >
-            2020-21
-          </button>
-        </li>
-      </ul>
-      <hr />
-      <div className="tab-content">
-        <div
-          className="tab-pane fade show active"
-          id="twentyfour"
-          role="tabpanel"
-          aria-labelledby="twentyfour-tab"
-        >
-          <div className="row">
-            <div className="col-sm-6 pt-2">
-              {matches[0].details.map((game: any, index: number) => {
-                return (
-                  <div className="card mb-3" key={game.date}>
-                    <ShowGameCardHeader
-                      date={game.date}
-                      venue={game.venue}
-                      scored={game.scored}
-                      conceded={game.conceded}
-                    />
-                    <div className="card-body">
-                      <ShowGameCardBody
-                        opposition={game.opposition}
-                        league={game.league}
-                        cards={game.cards}
-                        goals={game.goals}
-                        video={game.video}
-                        notes={game.notes}
-                      />
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-            <div className="col-sm-6 pt-2">
-              <ShowSeasonGoalScorers goalscorers={goals[0].details} />
-            </div>
-            <div className="col-sm-12 mb-4">
-              <a href="#top">Back to top</a>
-            </div>
-          </div>
+      <div className="row">
+        <div className="col-sm-6 pt-2">
+          {matches.map((game: any, index: number) => {
+            return (
+              <div className="card mb-3" key={game.date}>
+                <ShowGameCardHeader
+                  date={game.date}
+                  venue={game.venue}
+                  scored={game.scored}
+                  conceded={game.conceded}
+                />
+                <div className="card-body">
+                  <ShowGameCardBody
+                    opposition={game.opposition}
+                    league={game.league}
+                    cards={game.cards}
+                    goals={game.goals}
+                    video={game.video}
+                    notes={game.notes}
+                  />
+                </div>
+              </div>
+            );
+          })}
         </div>
-        <div
-          className="tab-pane fade"
-          id="twentythree"
-          role="tabpanel"
-          aria-labelledby="twentythree-tab"
-        >
-          <div className="row">
-            <div className="col-sm-6 pt-2">
-              {matches[1].details.map((game: any, index: number) => {
-                return (
-                  <div className="card mb-3" key={game.date}>
-                    <ShowGameCardHeader
-                      date={game.date}
-                      venue={game.venue}
-                      scored={game.scored}
-                      conceded={game.conceded}
-                    />
-                    <div className="card-body">
-                      <ShowGameCardBody
-                        opposition={game.opposition}
-                        league={game.league}
-                        cards={game.cards}
-                        goals={game.goals}
-                        video={game.video}
-                        notes={game.notes}
-                      />
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-            <div className="col-sm-6 pt-2">
-              <ShowSeasonGoalScorers goalscorers={goals[1].details} />
-            </div>
-            <div className="col-sm-12 mb-4">
-              <a href="#top">Back to top</a>
-            </div>
-          </div>
+        <div className="col-sm-6 pt-2">
+          <ShowSeasonGoalScorers goalscorers={goals} />
         </div>
-        <div
-          className="tab-pane fade"
-          id="twentytwo"
-          role="tabpanel"
-          aria-labelledby="twentytwo-tab"
-        >
-          <div className="row">
-            <div className="col-sm-6 pt-2">
-              {matches[2].details.map((game: any, index: number) => {
-                return (
-                  <div className="card mb-3" key={game.date}>
-                    <ShowGameCardHeader
-                      date={game.date}
-                      venue={game.venue}
-                      scored={game.scored}
-                      conceded={game.conceded}
-                    />
-                    <div className="card-body">
-                      <ShowGameCardBody
-                        opposition={game.opposition}
-                        league={game.league}
-                        cards={game.cards}
-                        goals={game.goals}
-                        video={game.video}
-                        notes={game.notes}
-                      />
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-            <div className="col-sm-6 pt-2">
-              <ShowSeasonGoalScorers goalscorers={goals[2].details} />
-            </div>
-            <div className="col-sm-12 mb-4">
-              <a href="#top">Back to top</a>
-            </div>
-          </div>
-        </div>
-        <div
-          className="tab-pane fade"
-          id="twentyone"
-          role="tabpanel"
-          aria-labelledby="twentyone-tab"
-        >
-          <div className="row">
-            <div className="col-sm-6 pt-2">
-              {matches[3].details.map((game: any, index: number) => {
-                return (
-                  <div className="card mb-3" key={game.date}>
-                    <ShowGameCardHeader
-                      date={game.date}
-                      venue={game.venue}
-                      scored={game.scored}
-                      conceded={game.conceded}
-                    />
-                    <div className="card-body">
-                      <ShowGameCardBody
-                        opposition={game.opposition}
-                        league={game.league}
-                        cards={game.cards}
-                        goals={game.goals}
-                        video={game.video}
-                        notes={game.notes}
-                      />
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-            <div className="col-sm-6 pt-2">
-              <ShowSeasonGoalScorers goalscorers={goals[3].details} />
-            </div>
-            <div className="col-sm-12 mb-4">
-              <a href="#top">Back to top</a>
-            </div>
-          </div>
+        <div className="col-sm-12 mb-4">
+          <a href="#top">Back to top</a>
         </div>
       </div>
     </>
---
+++ src/components/react/tabs.js
@@ -0,0 +1,39 @@
+import React, { useState, useCallback } from "react";
+
+function Tabs({ children }) {
+  const initialTab = children[0].props.label
+  const [activeTab, setActiveTab] = useState(initialTab)
+  const handleActiveTab = useCallback(label => setActiveTab(label), [])
+
+  const tabs = children.map(child => (
+    <button
+      onClick={e => {
+        e.preventDefault();
+        handleActiveTab(child.props.label);
+      }}
+      className={
+        child.props.label === activeTab
+          ? ["tabs__tab", "tabs__tab-active"].join(" ")
+          : "tabs__tab"
+      }
+      key={child.props.label}
+    >
+      {child.props.tabName}
+    </button>
+  ))
+  
+  const tabContent = children.filter(child => child.props.label === activeTab)
+
+  return (
+    <div>
+      <div className="tabs__box">{tabs}</div>
+      <div>{tabContent}</div>
+    </div>
+  )
+}
+
+function Tab(props) {
+	return <>{props.children}</>
+}
+
+export { Tabs, Tab }
